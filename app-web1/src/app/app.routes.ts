aff69a86f0a88d31f85b324e969a486e322e2948
AL <diego_cupitramo@fet.edu.co>
2026-08-26 12:21:55 -0500

    pagina api rick and morty

--- app-web1/src/app/app.routes.ts
+++ app-web1/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'episodes'
+  },
+  {
+    path: 'episodes',
+    loadComponent: () =>
+      import('./pages/episodes/episodes-page').then(
+        (m) => m.EpisodesPageComponent
+      )
+  },
+  {
+    path: '**',
+    redirectTo: 'episodes'
+  }
+];
